5a03eba96b1048a2e694d65e58de5895d40d3166
hamza5610 <hamzasiddique.idenbrid@gmail.com>
2024-01-17 16:05:27 +0500

    Banner files initialization

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Banner from '@/components/Home/Banner'
 import Header from '@/components/shared/Header/Header'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   )
 }
---
+++ src/components/Home/Banner.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Banner = () => {
+  return <>Banner here</>
+}
+
+export default Banner
