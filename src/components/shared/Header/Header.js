2e786cae79f09a4278fcbdcb1e8e177ea996a5d5
hamza5610 <hamzasiddique.idenbrid@gmail.com>
2024-01-17 15:51:31 +0500

    Logo added

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -8,6 +8,7 @@ import { FaBars } from 'react-icons/fa'
 import { usePathname } from 'next/navigation'
 import { useRouter } from 'next/navigation'
 import Sidebar from './Sidebar'
+import { ReactSVG } from 'react-svg'
 
 const Header = () => {
   const pathname = usePathname()
@@ -21,7 +22,10 @@ const Header = () => {
         <Container>
           <div className='flex justify-between items-center w-full'>
             <div className='w-1/2'>
-              <p className='text-white font-bold text-[24px] md:text-[32px]'>Logo Here</p>
+              <div className='flex gap-2 items-center'>
+                <ReactSVG src='/assets/logo.svg' className='h-6 w-6' />
+                <span className='text-white text-[24px] font-bold'>TripPix</span>
+              </div>
             </div>
             <div className='w-1/2 flex justify-end items-center gap-10'>
               <ul className='hidden md:flex items-center gap-6'>
