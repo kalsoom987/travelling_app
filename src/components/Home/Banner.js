34741e9eacdd498816479d2d1cb92cf1c68ea3f3
kalsoom987 <122975016+kalsoom987@users.noreply.github.com>
2024-01-19 12:19:59 +0500

    banner

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,7 +1,47 @@
-import React from 'react'
+'use client'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+import 'swiper/css/navigation'
+import 'swiper/css/autoplay'
+// import Container from '../shared/Reuseables/Container'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { EffectFade, Autoplay, Navigation, A11y } from 'swiper/modules'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 const Banner = () => {
-  return <>Banner here</>
+  const [show, setShow] = useState(false)
+  return (
+    // <Container>
+    <div className='absolute top-0 overflow-hidden h-[500px]'>
+      <Swiper
+        slidesPerView={1}
+        modules={[EffectFade, Autoplay, Navigation, A11y]}
+        loop
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={{ nextEl: '.arrow-left', prevEl: '.arrow-right' }}
+        effect='fade'>
+        <SwiperSlide>
+          <div className='w-full overflow-hidden h-[500] cover'>
+           
+            <Image src='/assets/images/banner2.jpg' alt='slide1' height={700} width={2600}   />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='w-full overflow-hidden  h-[500] cover'>
+            <Image src='/assets/images/banner3.jpg' alt='slide2' height={700} width={2600} />
+
+          </div>
+        </SwiperSlide>
+      </Swiper>
+     
+    </div>
+    // </Container>
+  )
 }
 
 export default Banner
