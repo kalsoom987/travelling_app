0ef9b9e2c761d136e8acd7a6ddc0736bce533fce
hamza7681 <hamzambf@gmail.com>
2024-01-20 10:26:00 +0500

    File issues fixed

--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,76 +1,84 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
-import Container from '../../shared/Reuseables/Container'
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
-import StepTwoForm from './StepTwoForm'
-import Link from 'next/link'
-import StepOneForm from './StepOneForm'
-import Finish from './Finish'
+import React, { useState } from "react";
+import Container from "../../shared/Reuseables/Container";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import StepTwoForm from "./StepTwoForm";
+import Link from "next/link";
+import FirstStepForm from "./FirstStepForm";
+import FinishComp from "./FinishComp";
 
 const Register = () => {
-  const [step, setStep] = useState(1)
-  const [genderDrop, setGenderDrop] = useState(false)
-  const [selectGen, setSelectGen] = useState('Select Gender')
-  const [dob, setDob] = useState(null)
-  const [togglePass, setTogglePass] = useState(false)
+  const [step, setStep] = useState(1);
+  const [genderDrop, setGenderDrop] = useState(false);
+  const [selectGen, setSelectGen] = useState("Select Gender");
+  const [dob, setDob] = useState(null);
+  const [togglePass, setTogglePass] = useState(false);
 
   const handleGender = (e, value) => {
-    e.stopPropagation()
-    setGenderDrop(false)
-    setSelectGen(value)
-  }
+    e.stopPropagation();
+    setGenderDrop(false);
+    setSelectGen(value);
+  };
 
   const handleStep = (type) => {
-    if (type === 'inc') {
-      setStep(() => step + 1)
-      window.scroll({ top: 0, left: 0, behavior: 'smooth' })
-    } else if (type === 'dec') {
+    if (type === "inc") {
+      setStep(() => step + 1);
+      window.scroll({ top: 0, left: 0, behavior: "smooth" });
+    } else if (type === "dec") {
       if (step > 0) {
-        setStep(() => step - 1)
-        window.scroll({ top: 0, left: 0, behavior: 'smooth' })
+        setStep(() => step - 1);
+        window.scroll({ top: 0, left: 0, behavior: "smooth" });
       }
     }
-  }
+  };
 
   return (
-    <div className='w-full flex justify-center items-center py-16'>
+    <div className="w-full flex justify-center items-center py-16">
       <Container>
-        <div className='flex flex-col items-center w-full'>
-          <p className='text-center text-black pb-2 text-3xl font-semibold'>Register</p>
-          <p className='pb-8'>
-            Already have an account?{' '}
-            <Link className='text-[#6553F7] font-semibold' href='/auth/login'>
-              Log in
-            </Link>
+        <div className="flex flex-col items-center w-full">
+          <p className="text-center text-black pb-2 text-3xl font-semibold">
+            Register
           </p>
-          <div className='flex w-full items-center justify-center'>
-            <p className='w-10 h-10 bg-[#6553F7] text-white flex justify-center items-center font-semibold rounded-full'>
+          {step < 3 && (
+            <p className="pb-8">
+              Already have an account?{" "}
+              <Link className="text-[#6553F7] font-semibold" href="/auth/login">
+                Log in
+              </Link>
+            </p>
+          )}
+          <div className="flex w-full items-center justify-center">
+            <p className="w-10 h-10 bg-[#6553F7] text-white flex justify-center items-center font-semibold rounded-full">
               1
             </p>
             <div
               className={`w-[100px] h-[2px] ${
-                step > 1 ? 'bg-[#6553F7]' : 'bg-[#d9d9d9]'
-              } duration-300 transition-all`}></div>
+                step > 1 ? "bg-[#6553F7]" : "bg-[#d9d9d9]"
+              } duration-300 transition-all`}
+            ></div>
             <p
               className={`w-10 h-10 ${
-                step > 1 ? 'bg-[#6553F7]' : 'bg-[#d9d9d9]'
-              } duration-300 transition-all  text-white flex justify-center items-center font-semibold rounded-full`}>
+                step > 1 ? "bg-[#6553F7]" : "bg-[#d9d9d9]"
+              } duration-300 transition-all  text-white flex justify-center items-center font-semibold rounded-full`}
+            >
               2
             </p>
             <div
               className={`w-[100px] h-[2px] ${
-                step > 2 ? 'bg-[#6553F7]' : 'bg-[#d9d9d9]'
-              } duration-300 transition-all`}></div>
+                step > 2 ? "bg-[#6553F7]" : "bg-[#d9d9d9]"
+              } duration-300 transition-all`}
+            ></div>
             <p
               className={`w-10 h-10 ${
-                step > 2 ? 'bg-[#6553F7]' : 'bg-[#d9d9d9]'
-              } text-white flex justify-center items-center font-semibold rounded-full duration-300 transition-all`}>
+                step > 2 ? "bg-[#6553F7]" : "bg-[#d9d9d9]"
+              } text-white flex justify-center items-center font-semibold rounded-full duration-300 transition-all`}
+            >
               3
             </p>
           </div>
           {step === 1 && (
-            <StepOneForm
+            <FirstStepForm
               selectGen={selectGen}
               startDate={dob}
               setStartDate={setDob}
@@ -82,19 +90,23 @@ const Register = () => {
             />
           )}
           {step === 2 && <StepTwoForm />}
-          {step === 3 && <Finish />}
+          {step === 3 && <FinishComp />}
           {step < 3 && (
-            <div className='w-full flex justify-between items-center'>
+            <div className="w-full flex justify-between items-center">
               <button
-                onClick={() => handleStep('dec')}
+                onClick={() => handleStep("dec")}
                 className={` ${
-                  step > 1 ? 'opacity-1 hover:bg-[#6553F7] hover:text-white' : 'opacity-[0.3] cursor-not-allowed'
-                } flex items-center  rounded-md px-4 py-2 justify-center text-[#333333] gap-2 duration-300 transition-all `}>
+                  step > 1
+                    ? "opacity-1 hover:bg-[#6553F7] hover:text-white"
+                    : "opacity-[0.3] cursor-not-allowed"
+                } flex items-center  rounded-md px-4 py-2 justify-center text-[#333333] gap-2 duration-300 transition-all `}
+              >
                 <FaChevronLeft /> <span>Back</span>
               </button>
               <button
-                onClick={() => handleStep('inc')}
-                className='flex items-center  rounded-md px-4 py-2 justify-center text-[#6553F7] gap-2 duration-300 transition-all hover:bg-[#6553F7] hover:text-white'>
+                onClick={() => handleStep("inc")}
+                className="flex items-center  rounded-md px-4 py-2 justify-center text-[#6553F7] gap-2 duration-300 transition-all hover:bg-[#6553F7] hover:text-white"
+              >
                 Continue <FaChevronRight />
               </button>
             </div>
@@ -102,7 +114,7 @@ const Register = () => {
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
