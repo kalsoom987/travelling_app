0f3b9baf9eec200ebbcebfe8f51dd87154d4447c
hamza5610 <hamzasiddique.idenbrid@gmail.com>
2024-01-31 16:56:38 +0500

    service module updated

--- src/components/Home/SearchTour.js
+++ src/components/Home/SearchTour.js
@@ -1,58 +1,129 @@
-import React from 'react'
-import Image from 'next/image'
+'use client'
 
+import React, { useState } from 'react'
+import { LuMapPin } from 'react-icons/lu'
+import Container from '../shared/Reuseables/Container'
 
 function SearchTour() {
-  
+  const [show, setShow] = useState('')
   return (
-    <div className='flex flex-row w-full' >
-      
-        <div className='grid  md:grid-cols-5 grid-cols-1  w-full '>
-
-
-          <div className='bg-[url("/assets/images/Azad.jpg")] bg-contain bg-no-repeat h-[300px] relative overflow-hidden'>
-            Kumrat Valley
-            <div className='bg-[rgba(0,0,0,0.7)]  bg-opacity-0 absolute top-0 h-[300px] w-full'>
-
+    <>
+      <div className='w-full flex items-center justify-center pt-[95px]'>
+        <Container>
+          <div className='flex flex-col mb-[50px]'>
+            <h1 className='flex items-center gap-3 text-[29px] mb-[22px]'>
+              <LuMapPin className='text-[#4296EE] text-[32px] ' />{' '}
+              <span className='text-[#333333]'>Popular Destinations</span>
+            </h1>
+            <p className='text-[#8c8c8c] text-[14px] pl-2'>
+              Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non
+              magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+            </p>
+          </div>
+        </Container>
+      </div>
+      <div className='flex flex-row w-full'>
+        <div className='md:px-0 px-4 py-4 grid md:grid-cols-5 grid-cols-1  w-full '>
+          <div
+            className='bg-[url("/assets/images/Azad.jpg")] bg-cover bg-no-repeat h-[300px] md:h-[380px] lg:h-[450px] relative overflow-hidden'
+            onMouseEnter={() => setShow('1st')}
+            onMouseLeave={() => setShow('')}>
+            <div
+              className={`absolute left-5 ${
+                show === '1st' ? 'bottom-4' : '-bottom-5'
+              } transition-all duration-300 z-[2] flex flex-col gap-4`}>
+              <p className='text-white font-semibold flex items-center gap-3'>
+                <LuMapPin /> <span>Kumrat Valley</span>
+              </p>
+              <p className='text-[#4296EE] text-xs font-semibold uppercase'>View All Tours</p>
             </div>
+            <div className='bg-black opacity-0 hover:opacity-[0.6] duration-300 transition-all absolute top-0 h-[300px] md:h-[380px] lg:h-[450px] w-full'></div>
+            <div
+              className={` ${
+                show === '1st' ? 'h-1' : 'h-0'
+              } transition-all duration-300 bg-[#4296EE] w-full absolute bottom-0`}></div>
           </div>
 
-          <div className='bg-[url("/assets/images/Kalam-Valley.jpg")] bg-contain bg-no-repeat h-[300px] relative overflow-hidden'>
-            Kalam Valley
-
-            <div className='bg-[rgba(0,0,0,0.7)]  bg-opacity-0 absolute top-0 h-[300px] w-full'>
-
+          <div
+            className='bg-[url("/assets/images/Kalam-Valley.jpg")] bg-cover bg-no-repeat h-[300px] md:h-[380px] lg:h-[450px] relative overflow-hidden'
+            onMouseEnter={() => setShow('2nd')}
+            onMouseLeave={() => setShow('')}>
+            <div
+              className={`absolute left-5 ${
+                show === '2nd' ? 'bottom-4' : '-bottom-5'
+              } transition-all duration-300 z-[2] flex flex-col gap-4`}>
+              <p className='text-white font-semibold flex items-center gap-3'>
+                <LuMapPin /> <span>Kalam Valley</span>
+              </p>
+              <p className='text-[#4296EE] text-xs font-semibold uppercase'>View All Tours</p>
             </div>
+            <div className='bg-black opacity-0 hover:opacity-[0.6] duration-300 transition-all absolute top-0 h-[300px] md:h-[380px] lg:h-[450px] w-full'></div>
+            <div
+              className={` ${
+                show === '2nd' ? 'h-1' : 'h-0'
+              } transition-all duration-300 bg-[#4296EE] w-full absolute bottom-0`}></div>
           </div>
-          <div className='bg-[url("/assets/images/Hunza-Valley.jpg")] bg-contain bg-no-repeat h-[300px] relative overflow-hidden '>
-            Hunza Valley
-            <div className='bg-[rgba(0,0,0,0.7)]  bg-opacity-0 absolute top-0  h-[300px] w-full'>
-
+          <div
+            className='bg-[url("/assets/images/Hunza-Valley.jpg")] bg-cover bg-no-repeat h-[300px] md:h-[380px] lg:h-[450px] relative overflow-hidden '
+            onMouseEnter={() => setShow('3rd')}
+            onMouseLeave={() => setShow('')}>
+            <div
+              className={`absolute left-5 ${
+                show === '3rd' ? 'bottom-4' : '-bottom-5'
+              } transition-all duration-300 z-[2] flex flex-col gap-4`}>
+              <p className='text-white font-semibold flex items-center gap-3'>
+                <LuMapPin /> <span>Hunza Valley</span>
+              </p>
+              <p className='text-[#4296EE] text-xs font-semibold uppercase'>View All Tours</p>
             </div>
+            <div className='bg-black opacity-0 hover:opacity-[0.6] duration-300 transition-all absolute top-0 h-[300px] md:h-[380px] lg:h-[450px] w-full'></div>
+            <div
+              className={` ${
+                show === '3rd' ? 'h-1' : 'h-0'
+              } transition-all duration-300 bg-[#4296EE] w-full absolute bottom-0`}></div>
           </div>
-          <div className='bg-[url("/assets/images/footer6.jpg")] bg-contain bg-no-repeat h-[300px] relative overflow-hidden'>
-            Kumrat Valley
-            <div className='bg-[rgba(0,0,0,0.7)]  bg-opacity-0 absolute top-0 h-[300px] w-full'>
-
+          <div
+            onMouseEnter={() => setShow('4th')}
+            onMouseLeave={() => setShow('')}
+            className='bg-[url("/assets/images/footer6.jpg")] bg-cover bg-no-repeat h-[300px] md:h-[380px] lg:h-[450px] relative overflow-hidden'>
+            <div
+              className={`absolute left-5 ${
+                show === '4th' ? 'bottom-4' : '-bottom-5'
+              } transition-all duration-300 z-[2] flex flex-col gap-4`}>
+              <p className='text-white font-semibold flex items-center gap-3'>
+                <LuMapPin /> <span>Islamabad City</span>
+              </p>
+              <p className='text-[#4296EE] text-xs font-semibold uppercase'>View All Tours</p>
             </div>
+            <div className='bg-black opacity-0 hover:opacity-[0.6] duration-300 transition-all absolute top-0 h-[300px] md:h-[380px] lg:h-[450px] w-full'></div>
+            <div
+              className={` ${
+                show === '4th' ? 'h-1' : 'h-0'
+              } transition-all duration-300 bg-[#4296EE] w-full absolute bottom-0`}></div>
           </div>
 
-          <div className='bg-[url("/assets/images/shadra.jpg")] bg-contain bg-no-repeat h-[300px] relative overflow-hidden'>
-            Sharda 
-            <div className='bg-[rgba(0,0,0,0.7)]  bg-opacity-0 absolute top-0 h-[300px] w-full '>
-
+          <div
+            onMouseEnter={() => setShow('5th')}
+            onMouseLeave={() => setShow('')}
+            className='bg-[url("/assets/images/shadra.jpg")] bg-cover bg-no-repeat h-[300px] md:h-[380px] lg:h-[450px] relative overflow-hidden'>
+            <div
+              className={`absolute left-5 ${
+                show === '5th' ? 'bottom-4' : '-bottom-5'
+              } transition-all duration-300 z-[2] flex flex-col gap-4`}>
+              <p className='text-white font-semibold flex items-center gap-3'>
+                <LuMapPin /> <span>Kaghan & Naran Valley</span>
+              </p>
+              <p className='text-[#4296EE] text-xs font-semibold uppercase'>View All Tours</p>
             </div>
-
+            <div className='bg-black opacity-0 hover:opacity-[0.6] duration-300 transition-all absolute top-0 h-[300px] md:h-[380px] lg:h-[450px] w-full'></div>
+            <div
+              className={` ${
+                show === '5th' ? 'h-1' : 'h-0'
+              } transition-all duration-300 bg-[#4296EE] w-full absolute bottom-0`}></div>
           </div>
-
-
-
-
         </div>
-      
-
-
-    </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Home/Service.js
+++ src/components/Home/Service.js
@@ -2,67 +2,43 @@ import React from 'react'
 import Image from 'next/image'
 import Container from '../shared/Reuseables/Container'
 
-
 function Service() {
-    return (
-
-        <div className='flex flex-row bg-[#468ae7] py-16 text-white w-full  items-center justify-center  space-x-[5px] '>
-            <Container>
-                <div className='grid  md:grid-cols-4 grid-cols-1 gap-[40px]'>
-
-                    <div className='items-center justify-start flex  gap-4 '>
-
-                        <Image src='/assets/icons/icon-1.png' alt='service' width={49} height={49} />
-                        <div >
-                            <h2>
-                                +100 Destinations
-                            </h2>
-                            <span className='text-[#94a3b8]'>
-                                Chitaral,Hunza valley,swat valley
-                            </span>
-                        </div>
-                    </div>
-                    <div className='items-center justify-start flex  gap-4 '>
-
-                        <Image src='/assets/icons/icon-2.png' alt='service' width={49} height={49} />
-                        <div >
-                            <h2>
-                                Best price
-                            </h2>
-                            <span className='text-[#94a3b8]'>
-                                special offers and discounts
-                            </span>
-                        </div>
-                    </div>
-                    <div className='items-center justify-start flex  gap-4 '>
-
-                        <Image src='/assets/icons/icon-3.png' alt='service' width={49} height={49} />
-                        <div >
-                            <h2>
-                                Great Customer
-                            </h2>
-                            <span className='text-[#94a3b8]'>
-                                we have a great customer
-                            </span>
-                        </div>
-                    </div>
-                    <div className='items-center justify-start flex  gap-4 '>
-
-                        <Image src='/assets/icons/icon-4.png' alt='service' width={49} height={49} />
-                        <div >
-                            <h2>
-                                Fast booking
-                            </h2>
-                            <span className='text-[#94a3b8]'>
-                                online booking
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </Container>
+  return (
+    <div className='flex flex-row bg-[#468ae7] py-16 text-white w-full  items-center justify-center  space-x-[5px] '>
+      <Container>
+        <div className='grid  md:grid-cols-4 grid-cols-1 gap-5'>
+          <div className='items-center justify-start flex  gap-4 '>
+            <Image src='/assets/icons/airplane.png' alt='service' width={49} height={49} />
+            <div>
+              <h2 className='text-sm text-white'>+100 Destinations</h2>
+              <span className='text-[#b9cdf3] text-sm'>Enjoy your tours</span>
+            </div>
+          </div>
+          <div className='items-center justify-start flex  gap-4 '>
+            <Image src='/assets/icons/cash.png' alt='service' width={49} height={49} />
+            <div>
+              <h2>Best price</h2>
+              <span className='text-[#b9cdf3] text-sm'>special offers and discounts</span>
+            </div>
+          </div>
+          <div className='items-center justify-start flex  gap-4 '>
+            <Image src='/assets/icons/support.png' alt='service' width={49} height={49} />
+            <div>
+              <h2>Great Customer</h2>
+              <span className='text-[#b9cdf3] text-sm'>we have a great customer</span>
+            </div>
+          </div>
+          <div className='items-center justify-start flex  gap-4 '>
+            <Image src='/assets/icons/schedule.png' alt='service' width={49} height={49} />
+            <div>
+              <h2>Fast booking</h2>
+              <span className='text-[#b9cdf3] text-sm'>online booking</span>
+            </div>
+          </div>
         </div>
-
-    )
+      </Container>
+    </div>
+  )
 }
 
 export default Service
